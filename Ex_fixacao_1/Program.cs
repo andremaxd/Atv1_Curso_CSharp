9504948ec78550eff137d9b6e02284f304eaa11a
André <andremaxd12@gmail.com>
2020-06-28 22:05:20 -0300

    Exercicio pronto

---
+++ Ex_fixacao_1/Program.cs
@@ -0,0 +1,66 @@
+﻿using Ex_fixacao_1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Ex_fixacao_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<=n; i++)
+            {
+                Console.WriteLine($"Product #{i} date:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char ch = char.Parse(Console.ReadLine());
+                if(ch == 'c')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Product(name, price));
+                } 
+                else if(ch == 'u')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+                else if(ch == 'i')
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Customs fee: ");
+                    double customs = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customs));
+                }
+                else
+                {
+                    Console.WriteLine("Caracter inválido!");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach(Product prod in list)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+
+
+        }
+    }
+}
